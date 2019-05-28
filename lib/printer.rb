e53d5351ebc9a575db7cf01f1710fb4254c0d493
NicGiles <n.r.giles1@gmail.com>
2019-05-28 12:24:06 +0100

    Created unique visitor count method within printer class.

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,6 +1,11 @@
 # printer class for displaying number of times sites have been visited.
 class Printer
+
   def site_count(sites)
     sites.each { |site, visits| puts "#{site} has #{visits.length} visits." }
+  end
+
+  def unique_visitor_count(ips)
+    ips.each { |site, visitors| puts "#{site} has been visited by #{visitors.uniq.length} people or bots." }
   end
 end
