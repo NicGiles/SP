4545c52a0705cf0e3dd360f4c61ff92bc0d827c2
NicGiles <n.r.giles1@gmail.com>
2019-05-28 12:06:47 +0100

    Created web_logs class and test_log file. Ran Rubocop.

---
+++ lib/web_logs.rb
@@ -0,0 +1,16 @@
+# web_logs class for storing log into a hash.
+class Web_logs
+  attr_reader :sitelist
+
+  def initialize
+    @sitelist = {}
+  end
+
+  def create_sites_hash(filename)
+    File.open(filename).each do |ip_site_logs|
+      sites = ip_site_logs.split(' ')
+      @sitelist.key?(sites[0]) ?
+      @sitelist[sites[0]] << (sites[1]) : @sitelist[sites[0]] = [sites[1]]
+    end
+  end
+end
--- spec/web_logs_spec.rb
+++ spec/web_logs_spec.rb
@@ -4,11 +4,10 @@ require 'spec_helper'
 describe Web_logs do
   subject(:logs) { described_class.new }
 
-
-    describe '#create_sites_hash' do
-      it 'Takes a log of files and saves them into a hash.' do
-        subject.create_sites_hash("./spec/test_log.log")
-        expect(subject.sitelist).to eq ({"contact"=>["184"], "home"=>["184", "235", "185"], "/about/2"=>["444", "444"], "page1"=>["929", "722", "646", "126"], "/index"=>["444", "445", "446", "447"], "/about"=>["061"]})
-      end
+  describe '#create_sites_hash' do
+    it 'Takes a log of files and saves them into a hash.' do
+      subject.create_sites_hash('./spec/test_log.log')
+      expect(subject.sitelist).to eq ({ 'contact' => ['184'], 'home' => %w[184 235 185], '/about/2' => %w[444 444], 'page1' => %w[929 722 646 126], '/index' => %w[444 445 446 447], '/about' => ['061'] })
     end
   end
+end
